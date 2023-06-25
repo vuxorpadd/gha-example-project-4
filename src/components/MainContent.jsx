d596c68b6bd8ac512e3224f8125dd06f2d1da2ef
pavlo.zaporozkyi <pavlo.zaporozkyi@mev.com>
2023-06-25 13:03:47 +0300

    Break

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -11,7 +11,7 @@ function MainContent() {
 
   return (
     <main>
-      <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
+      <div onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</div>
       {helpVisible && <HelpArea />}
     </main>
   );
